bf503076a35e10b46ea04254c61a3bf4f5528e8a
abdulSalamKagaji97 <98615380+abdulSalamKagaji97@users.noreply.github.com>
2026-01-13 15:32:08 -0800

    update logs

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ from app import app, socketio
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
+    print(f"Starting server on 0.0.0.0:{port}", flush=True)
     socketio.run(app, host="0.0.0.0", port=port)
