0220e93c30c96add6898fe5d254b89594b15059d
abdulSalamKagaji97 <98615380+abdulSalamKagaji97@users.noreply.github.com>
2026-01-13 12:49:57 -0800

    idk

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-from .game_engine.game_state import GameManager
-from .storage.game_store import GameStore
-from .sockets.events import register_events
+try:
+    from .game_engine.game_state import GameManager
+    from .storage.game_store import GameStore
+    from .sockets.events import register_events
+except ImportError:
+    from game_engine.game_state import GameManager
+    from storage.game_store import GameStore
+    from sockets.events import register_events
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
@@ -13,6 +18,4 @@ register_events(socketio, manager, store)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=8000)
+application = app
