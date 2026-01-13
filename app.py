756c824658832dc56a5300a317331ba6d68cf4e0
abdulSalamKagaji97 <98615380+abdulSalamKagaji97@users.noreply.github.com>
2026-01-13 12:42:50 -0800

    fix event let invokation

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import eventlet
-eventlet.monkey_patch()
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-from game_engine.game_state import GameManager
-from storage.game_store import GameStore
-from sockets.events import register_events
+from .game_engine.game_state import GameManager
+from .storage.game_store import GameStore
+from .sockets.events import register_events
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
